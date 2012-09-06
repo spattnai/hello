ab9c0699b69261b93610eaa922df27c2c8ba7dd3
spattnai <spattnai@macalester.edu>
2012-09-05 20:37:45 -0500

    Update helloworld/src/helloworld/Hello.java

--- helloworld/src/helloworld/Hello.java
+++ helloworld/src/helloworld/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String args[]){
 		System.out.println("Hello world");
 		System.out.println("My name is Shivangi and I love everything about computers");
+		System.out.println("This is to try out fetching");
+		System.out.println("This worked");
 	}
 
 }
