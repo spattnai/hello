55b9f6203028432cc4e367dbe1ec85b74fe99677
Shivangi <Shivangi@141.140.157.138>
2012-09-05 20:32:32 -0500

    first commit on juno

---
+++ helloworld/src/helloworld/Hello.java
@@ -0,0 +1,9 @@
+package helloworld;
+
+public class Hello {
+	public static void main(String args[]){
+		System.out.println("Hello world");
+		System.out.println("My name is Shivangi and I love everything about computers");
+	}
+
+}
